fb84dbac20f4907506389a5e13fa2cae57c7a772
Hugo Alex <hi@hugoalex.com>
2014-06-05 21:16:39 -0300

    commit inicial

---
+++ LogicaParaconsistente/slide_config.js
@@ -0,0 +1,43 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Lógica Paraconsistente e Quântica',
+    course: 'Lógica Aplicada à Computação',
+    teacher: 'Professor: Fabio Paraguaçu',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/touch_icon.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  }, 
+  // Author information
+  presenters: [{
+    name: 'Hugo Alex',
+    company: 'Estudante, UFAL',
+    gplus: 'https://plus.google.com/u/0/+HugoAlexps',
+    twitter: '@hug0alex',
+    www: 'http://www.hugoalex.com',
+    github: 'http://github.com/hug0alex'
+  }, {
+    name: 'Luiz Matheus',
+    company: 'Estudante, UFAL',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  },{
+    name: 'Lucas Due',
+    company: 'Estudante, UFAL',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }]
+};
+
